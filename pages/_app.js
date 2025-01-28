6019c7d441c5f835cb6e8ad01631a7978d488dde
Adib Khan <khanadib418@gmail.com>
2025-01-28 12:14:20 +0400

    committed some changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
-import "@/styles/globals.css"
-import { SessionProvider } from "next-auth/react"
+import "@/styles/globals.css";
+import { SessionProvider } from "next-auth/react";
+import Head from "next/head";
 
-export default function App({Component, pageProps: { session, ...pageProps },})
-{
-  return (
-      <SessionProvider session={session}>
-        <Component {...pageProps} />
-      </SessionProvider>
-  )
+export default function App({ Component, pageProps: { session, ...pageProps } }) {
+    return (
+        <SessionProvider session={session}>
+            <Head>
+                <title>Next.js Authentication</title>
+                <meta name="description" content="Next.js Authentication" />
+            </Head>
+            <Component {...pageProps} />
+        </SessionProvider>
+    );
 }
